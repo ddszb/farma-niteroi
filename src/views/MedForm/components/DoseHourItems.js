29fe2937c4a0c6f233c7ab34c63479368e06ca52
ddszb <zambranobrasil@gmail.com>
2021-06-27 22:49:53 -0300

    Ajustando componente de lista de doses

--- src/views/MedForm/components/DoseHourItems.js
+++ src/views/MedForm/components/DoseHourItems.js
@@ -23,9 +23,20 @@ export default props =>{
         selectedDose: 0,
         showDialog: false,
         showDoseHourPicker: false,
+        dialogTime: defaultDate
     })
 
-    const __updateDoseTime = (event, date) =>{
+    /**
+     * Atualiza a hora da dose selecionada
+     * @param {String} event Tipo do evento de ação do DatePicker
+     * @param {Date} date Data selecionada
+     * @returns 
+     */
+    const __updateDoseItem = (event, date) =>{
+        if(date === undefined){
+            setState({...state, showDoseHourPicker: false})
+            return
+        }
         if(event.type === "set"){
             // Atualizando dose selecionada
             var doses = state.doseHoursItems
@@ -35,8 +46,9 @@ export default props =>{
             
             // Atualizando lista
             doses.splice(index, 1, selectedDose)
-            // if(dose.index == 0){
+            // if(doses[index].index == 0 && doses.length > 1){
             //     doses = __shiftDoseTimes(doses)
+            //     console.warn("novo doses", doses.map(p=>p.time.getHours()))
             // }
             
             // Atualizando estado
@@ -44,8 +56,10 @@ export default props =>{
                 ...state,
                 selectedDose: index,
                 doseHoursItem: doses,
+                dialogTime: date,
                 showDoseHourPicker: false,
             })
+            console.warn("BC:", state.dialogTime, doses )
         }else{
             setState({
                 ...state,
@@ -55,29 +69,24 @@ export default props =>{
         
     }
 
-    const __shiftDoseTimes = (doses) =>{
-        const newDoses = []
-        offset = 8 - doses[0].getHours()
-        for(var i = 0; i < doses.length; i++){
-            let dose = doses[i]
-            dose.setHours(doses[i] + offset >= 24 ? doses[i] + offset : 24 - doses[i] + offset)
-            newDoses.push(dose)
-        }
-        return newDoses
-    }
-
-    const __createDoseTimes = (value) =>{
-        var defaultStartHour = 8
-        var defaultStartMinute = 0
-        var amountInADay = value;
+    /**
+     * Atualiza o horário das doses a serem tomadas de acordo
+     * com o horário inicial passado como parâmetro
+     * @param {Number} amountInADay quantidade de doses no dia
+     * @param {Number} startHour horário inicial
+     * @param {Number} startMinute minuto iniciais
+     * @returns 
+     */
+    const __updateDoseTimes = (amountInADay, startHour, startMinute) =>{
+        console.warn("startHour", startHour)
         var interval = 24 / amountInADay;
         
         doseHours = []
         for( i = 0; i < amountInADay; i++){
-            let startTime = defaultStartHour + (i * interval)
+            let startTime = startHour + (i * interval)
             let doseTime = new Date()
             doseTime.setHours(startTime)
-            doseTime.setMinutes(defaultStartMinute)
+            doseTime.setMinutes(startMinute)
             let doseHour = {
                 time: doseTime,
                 amount: 1,
@@ -101,7 +110,12 @@ export default props =>{
             doseHoursItems: doseHours,
         })
     }
-
+        
+    /**
+     * Atualiza informações de quantidade e unidade de medida de uma dose
+     * @param {Number} amount A quantidade a ser tomada na dose
+     * @param {String} unit A unidade de medida da dose
+     */
     const __updateItem = (amount, unit) =>{
         var doses = state.doseHoursItems
         var index = state.selectedDose
@@ -120,20 +134,56 @@ export default props =>{
         })
     }
 
+    /**
+     * Atualiza o estado para não mostrar o dialog de unidade de dosagem
+     */
     const __closeDialog = () =>{
         setState({ ...state, showDialog: false})
     }
 
+    /**
+     * Atualiza o horário das doses para sugerir um novo cronograma a 
+     * partir do horário da primeira dose da lista
+     * @param {Array} doses A lista das doses que devem ser atualizadas
+     * @returns A lista com horários ajustados
+     */
+    const __shiftDoseTimes = (doses) =>{
+        const newDoses = []
+        var offset = doses[0].time.getHours() - 8
+        for(var i = 0; i < doses.length; i++){
+            let dose = doses[i]
+            let hours = dose.time.getHours()
+            dose.setHours( hours + offset < 24 ? hours + offset : 24 - hours + offset)
+            newDoses.push(dose)
+        }
+        return newDoses
+    }
+    
+    /**
+     * Cria a lista de doses de acordo com a opção selecionada no spinner
+     * @param {Number} value Quantidade de doses que serão tomadas em um dia
+     */
+    const __createDoseTimes = (value) =>{
+        // Cria doses com horário inicial padrão de 8:00
+        __updateDoseTimes(value, 8, 0)
+    }
+
+    /**
+     * Retorna o JSX da lista de doses para renderização em tela
+     * @returns JSX da lista
+     */
     const __doseHoursItemList = () =>{
         const doses = state.doseHoursItems
         return doses.map( d =>{
             return (
-                <>
-                    <View key={d.index} style={{flexDirection : 'row', justifyContent: 'space-between', padding: 0}}>
+                <View key={d.index}>
+                    <View style={{flexDirection : 'row', justifyContent: 'space-between', padding: 0}}>
                         <TouchableOpacity
-                            onPress={ () => { setState({
+                            onPress={ () => {
+                                setState({
                                 ...state,
                                 selectedDose: d.index,
+                                dialogTime: d.time,
                                 showDoseHourPicker: true,
                             })}}
                             >
@@ -160,25 +210,28 @@ export default props =>{
                         </TouchableOpacity>
                         { state.showDoseHourPicker && 
                         (<DateTimePicker
-                            value={d.time}
+                            value={state.dialogTime}
                             mode="time"
                             is24Hour={true}
                             display="spinner"
                             textColor="#63488c"
                             minuteInterval={5}
-                            onChange={__updateDoseTime}
-                            />)}
+                            onChange={__updateDoseItem}/>)
+                        }
                     </View>
                     <DoseHourDialog 
                         visible={state.showDialog} 
                         dose={d}
                         close={__closeDialog}
                         onSet={__updateItem}/>
-                </>
+                </View>
             )
         })
     }
 
+    /**
+     * Renderização da tela
+     */
     return(
         <>
             <TreatmentSpinner items={doseTimes} onChangeValue={__createDoseTimes}/> 
