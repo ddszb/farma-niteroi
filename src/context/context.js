76b834226ceea64ee1417680fe0a067b47668ed3
ddszb <zambranobrasil@gmail.com>
2021-07-15 18:09:57 -0300

    Removendo mock data

--- src/context/context.js
+++ src/context/context.js
@@ -1,5 +1,4 @@
 import React, {createContext, useReducer} from 'react'
-import meds from '../data/meds'
 import polis from '../data/polis'
 import medsNiteroi from '../data/meds_niteroi'
 import firestore from '@react-native-firebase/firestore'
@@ -13,34 +12,12 @@ import firestore from '@react-native-firebase/firestore'
     UseReducer (Hook): Gerenciamento do contexto, para evoluir o contexto
 
 */}
-const initialState = { meds, polis, medsNiteroi }
+const initialState = {polis, medsNiteroi }
 
 const AppContext = createContext({})
 
 const actions = {
 
-    updateMed(state, action){
-        const medUpdated = action.payload
-        return {
-            ...state,
-            meds: state.meds.map( u => u.id === medUpdated.id ? medUpdated : u)
-        }
-        
-    },
-
-    createMed(state, action){
-        const med = action.payload
-        med.id =  Math.random() * 100
-        return {
-            ...state,
-            meds: [...state.meds, med],
-        }
-    },
-    deleteMed(state, action){
-        const med = action.payload
-        return { ...state,
-                meds: state.meds.filter( x => x.id !== med.id)}
-    }
 }
 
 export const AppProvider = props =>{
