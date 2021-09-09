5346df7472fa119532a385d8f64d7747878f7607
ddszb <zambranobrasil@gmail.com>
2021-09-09 19:14:17 -0300

    Removendo funcionalidade de tratamento contínuo temporariamente

--- src/views/MedForm/components/DurationRadioGroup.js
+++ src/views/MedForm/components/DurationRadioGroup.js
@@ -1,9 +1,8 @@
 import React, {useState} from 'react'
-import {TextInput} from 'react-native'
 
-import {RadioButton, Text} from 'react-native-paper'
+import {Text} from 'react-native-paper'
 
-import {RadioButtonView, BorderlessInputText} from '../styles'
+import {RadioButtonView, BorderInputText} from '../styles'
 
 export default props =>{
 
@@ -15,25 +14,48 @@ export default props =>{
     props.onChangeValue(isOngoing ? 0 : days)
   }
 
-  return (
+  const onChangeDays = days =>{
+    days = days.replace(/[^0-9]/g, '')
+    if( +days <= 0 ){
+      days = ""
+    }
+    if( + days > 365){
+      days = "365"
+    }
+    setDays(days)
+    props.onChangeValue(days)
+  }
 
-      <RadioButton.Group 
-        onValueChange={__onValueChange} 
-        value={ongoing}>
-        <RadioButtonView>
-          <RadioButton value={false} />
-          <Text>Número de dias</Text>
-          { !ongoing &&
-          <BorderlessInputText
-            style={{marginLeft: 7}}
-            onChangeText={ days => setDays(days)}
-            value={'' + days}
-            keyboardType="numeric"/>}
-        </RadioButtonView>
-        <RadioButtonView>
-          <RadioButton value={true} />
-          <Text>Tratamento contínuo</Text>
-        </RadioButtonView>
-      </RadioButton.Group>
+  return (
+    <RadioButtonView>
+    <Text>Número de dias</Text>
+    { !ongoing &&
+    <BorderInputText
+      maxLength={3}
+      style={{marginLeft: 7}}
+      onChangeText={ onChangeDays}
+      value={'' + days}
+      keyboardType="numeric"/>}
+    </RadioButtonView>
+
+      // Funcionalidade a ser implementada no futuro
+      // <RadioButton.Group 
+      //   onValueChange={__onValueChange} 
+      //   value={ongoing}>
+      //   <RadioButtonView>
+      //     <RadioButton value={false} />
+      //     <Text>Número de dias</Text>
+      //     { !ongoing &&
+      //     <BorderlessInputText
+      //       style={{marginLeft: 7}}
+      //       onChangeText={ days => setDays(days)}
+      //       value={'' + days}
+      //       keyboardType="numeric"/>}
+      //   </RadioButtonView>
+      //   <RadioButtonView>
+      //     <RadioButton value={true} />
+      //     <Text>Tratamento contínuo</Text>
+      //   </RadioButtonView>
+      // </RadioButton.Group>
   )
 }
