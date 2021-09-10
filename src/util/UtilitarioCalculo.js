08d70345fe90cb80bc0597c0c58f813bde2b5a4a
ddszb <zambranobrasil@gmail.com>
2021-09-09 22:18:18 -0300

    Inserindo calculo de estoque para doses liquidas (gotas)

--- src/util/UtilitarioCalculo.js
+++ src/util/UtilitarioCalculo.js
@@ -1,4 +1,4 @@
-export const diffDays = (d1, d2) =>{
+export function diffDays(d1, d2){
         var _MS_PER_DAY = 1000 * 60 * 60 * 24
         var startDate = new Date(d1)
         var endDate = new Date(d2)
@@ -6,3 +6,21 @@ export const diffDays = (d1, d2) =>{
         const utc2 = Date.UTC(endDate.getFullYear(), endDate.getMonth(), endDate.getDate())
         return Math.floor((utc2 - utc1) / _MS_PER_DAY)
 }
+
+export function newStockAfterDose(med, dose){
+        dropsToMlRate = 20
+        let currStock = med.stock.amount
+        if(med.stock.unit.value == "ML" && dose.unit.liquid){
+                if(dose.unit.value === "GOTA"){
+                        var mlTaken = dose.amount / dropsToMlRate
+                        return + Math.max(currStock - mlTaken, 0)
+                }else{
+                        return + Math.max(currStock - dose.amount)
+                }
+        }else{
+                return + Math.max(currStock - dose.amount)
+        }
+}
+
+
+
