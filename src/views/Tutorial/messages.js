9da4fc6b04758d0f0ada07ee58b3a14cd7c63338
ddszb <zambranobrasil@gmail.com>
2021-10-12 16:54:51 -0300

    Nova tela de tutorial

---
+++ src/views/Tutorial/messages.js
@@ -0,0 +1,40 @@
+export default [
+        {
+            img: null,
+            text: "\t\tControle o seu estoque pessoal de medicamentos e horário de medicação facilmente através desse aplicativo.\n\n" +
+            "\t\tAlém disso, nele você encontra a relação de medicamentos oferecidos gratuitamente pela Prefeitura de Niterói e também os postos de saúde onde pode encontrá-los.",
+            title: "Bem vindo!",
+            welcome: true,
+        },
+        {
+            img: require('./1.gif'),
+            text: "Na tela inicial encontram-se as doses marcadas para cada dia. Para ver as doses de algum dia específico, " +
+            "basta tocar nas setas ou na data para selecionar uma data qualquer. Para voltar ao dia atual, basta clicar no botão \"hoje\".",
+        },
+        {
+            img: require('./2.gif'),
+            text: "Para marcar uma dose como tomada, basta tocar nela e confirmar. Para ajustar uma dose, basta tocar em \"Ajustar\", corrigir as informações e confirmar. \n" +
+            "No momento, só é possível marcar doses tomadas no dia atual."
+        },
+        {
+            img: require('./3.gif'),
+            text: "Para registrar uma nova dose eventual, basta tocar em \"Nova Dose\" e preencher as informações corretamente. Para isso, é necessário ter pelo menos um medicamento cadastrado.",
+        },
+        {
+            img: require('./4.gif'),
+            text: "Para adicionar um novo medicamento, vá para a aba \"Medicamentos\", toque no botão \"Novo medicamento\" e preencha as informações corretamente.",
+        },
+        {
+            img: require('./5.gif'),
+            text: "Para buscar os medicamentos oferecidos pela Prefeitura e as policlínicas participantes, vá para a aba \"Procurar\"",
+        },
+        {
+            img: require('./6.gif'),
+            text: "Para acessar dicas de saúde sobre como armazenar e consumir seus medicamentos corretamente, vá para a aba \"Dicas de Saúde\".",
+        },
+        {
+            img: require('./7.gif'),
+            text: "Também possível acessar este tutorial e todas as outras páginas novamente tocando no ícone superior esquerdo ou deslizando a partir do.",
+        },
+    
+]
