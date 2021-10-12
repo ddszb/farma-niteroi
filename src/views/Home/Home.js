df52f7181cc46a9e745d39fabfaf344a9cd6b78b
ddszb <zambranobrasil@gmail.com>
2021-10-12 19:50:41 -0300

    Refatoração da Home (Doses)

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,67 +1,58 @@
-import React, {useState, useEffect,  useRef} from 'react'
+import React, {useState, useEffect} from 'react'
 
-import { TouchableOpacity, RefreshControl, ToastAndroid } from 'react-native'
-import { createIconSetFromIcoMoon } from 'react-native-vector-icons'
-import iconMoonConfig from '../../selection.json'
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import { Icon } from 'react-native-elements/dist/icons/Icon'
-import { Body, CardBox, ColorTag, Container,
-     Detail, Buttons, DarkText, LightText, ResetDateButton,
-     TopContent, BottomContent, DateText, 
-     DatePickerView, EmptyListContainer, RightSwipe, RightSwipeText,
-     WarningText, 
-     RowView,
-     HPadding,
-     OkText,
-     WaitingText,
-     TopView,
-     ResetDatetext,
-     DateFilterContainer} from './styles'
-import { FlatList, Swipeable } from 'react-native-gesture-handler'
-import DateTimePicker from '@react-native-community/datetimepicker'
-
+// Libs e Hooks
 import {useFocusEffect} from '@react-navigation/native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import moment from 'moment'
 import 'moment/locale/pt-br'
+// Estilos
+import { Container, TopView} from './styles'
+// Enums
 import doseStatus from '../../constants/doseStatus'
-import EditDoseModal from './components/EditDoseModal'
 import doseActions from '../../constants/doseActions'
 import storageKeys from '../../constants/storageKeys'
+//Utilitário
 import * as Calculate from '../../util/UtilitarioCalculo'
-import colors from '../../styles/colors'
+// Componentes
+import EditDoseModal from './components/EditDoseModal'
 import Header from '../../components/Header'
-import { color } from 'react-native-reanimated'
 import PrimaryButton from '../../components/PrimaryButton'
+import DoseFilter from '../../components/Filter'
+import DateFilter from './components/DateFilter'
+import DoseList from './components/DoseList'
 
-
-const filterOptions = { ALL: 0, TAKEN: 1, NOT_TAKEN: 2}
-// const filterIcons = ['alarm-plus', 'alarm-check', 'alarm-off']
-const filterMsgs = ['Exibindo todas as doses', 'Exibindo apenas doses tomadas', 'Exibindo apenas doses não tomadas',]
+const filterOptions = { ALL: '0', NOT_TAKEN: '1', TAKEN: '2'}
 
 export default props =>{
 
-    const MedIcon = createIconSetFromIcoMoon(iconMoonConfig)
-
     const [meds, setMeds] = useState([])
     const [visibleDoses, setVisibleDoses] = useState([])
-    const [filterOption, setFilterOption] = useState(filterOptions.ALL)
-    const [showDatePicker, setShowDatePicker] = useState(false)
+    const [filterOption, setFilterOption] = useState('0')
+    const [showFilter, setShowFilter] = useState(false)
     const [filterDay, setFilterDay] = useState(new Date())
-    const [refreshing, setRefreshing] = useState(false)
     const [editedDose, setEditedDose] = useState()
     const [showEditDoseModal, setShowEditDoseModal] = useState(false)
-    const swipeableRef = useRef(null);
     
+    /**
+     * (testes) Limpa o local storage
+     */
     clearAsyncStorage = async() => {
         AsyncStorage.clear();
         setMeds([])
         setVisibleDoses([])
     }
 
+    /**
+     * Persiste os medicamentos atualizados.
+     */
     const updateMeds = async () =>{
         AsyncStorage.setItem(storageKeys.MEDS, JSON.stringify(meds))
     }
 
+    /**
+     * Verifica no local storage se é o primeiro acesso do usuário. 
+     * Se sim, direciona para a tela de tutorial.
+     */
     const checkFirstTime = async () =>{
         const hideTutorial = await AsyncStorage.getItem(storageKeys.FIRST_LOGIN)
         if(!hideTutorial){
@@ -69,55 +60,56 @@ export default props =>{
         }
     }
 
+    /**
+     * Carrega os medicamentos da fonte de dados.
+     */
     const getMeds = async () =>{
         const medsString = await AsyncStorage.getItem(storageKeys.MEDS)
         const meds = JSON.parse(medsString) || []
         setMeds(meds)
     }
 
-    const closeEditModal = () =>{
-        setShowEditDoseModal(false)
-    }
-
-
+    /**
+     * Callback para quando a tela entra em foco para carregamento de dados.
+     */
     useFocusEffect(
         React.useCallback(() =>{
         getMeds()
         checkFirstTime()
     }, []))
 
+    /**
+     * Atualiza a lista de doses visíveis sempre que há uma atualização na lista de medicamentos,
+     * data filtrada ou opção de filtro selecionada.
+     */
     useEffect(() =>{
         if((meds && meds.length > 0) && filterDay && filterOption != null){
             filterDoses()
         }
     }, [meds, filterDay, filterOption])
 
-    const onRefresh = () =>{
-        getMeds()
-        setRefreshing(false)
-    }
-
+    /**
+     * Navega para a tela de Adicionar Dose.
+     */
     const navigateToNew = () =>{
         props.navigation.navigate('Adicionar Dose', {screen: 'Adicionar Dose', meds: meds})
     }
 
-    const toggleFilter = () =>{
-        let n = filterOption == 2 ? 0 : filterOption + 1
-        setFilterOption(n)
-        ToastAndroid.showWithGravityAndOffset(filterMsgs[n], ToastAndroid.SHORT, ToastAndroid.TOP, 0 , 30)        
+    /**
+     * Retorna as opções disponíveis para o filtro de doses visíveis.
+     * @returns A lista com as opções
+     */
+    const getFilterOptions = () =>{
+        return ["Todas as doses", "Apenas não tomadas", "Apenas tomadas"]
     }
 
-    const filterDoses = (reorder) =>{
+    /**
+     * Seleciona as doses vísiveis de acordo com a data e opção de filtro selecionados
+     */
+    const filterDoses = () =>{
         var allDoses = [].concat.apply([], meds.map( m => m.doses)) // Concatena todas as doses de todos os medicamentos
-        let dayDoses = allDoses.filter( d => (moment(d.date).isSame(filterDay, 'day') || moment(d.dateTaken).isSame(filterDay, 'day')) && d.status !== doseStatus.ENCERRADA)
-        let taken = dayDoses.filter( d => d.dateTaken)
-        let notTaken = dayDoses.filter( d => !d.dateTaken)
-        if(reorder){
-            notTaken.sort((a, b) => new Date(a.date) - new Date(b.date))
-            taken.sort((a,b) => new Date(a.dateTaken) - new Date(b.dateTaken))
-        }
-        let visible = notTaken.concat(taken)
-
+        let visible = allDoses.filter( d => (moment(d.date).isSame(filterDay, 'day') || moment(d.dateTaken).isSame(filterDay, 'day')) && d.status !== doseStatus.ENCERRADA)
+        visible = visible.sort((a,b) => a.dateTaken ? a.dateTaken : a.date  - b.dateTaken ? b.dateTaken : b.date)
         if(filterOption == filterOptions.TAKEN){
             visible = visible.filter( d => d.status == doseStatus.TOMADA )
         }else if(filterOption == filterOptions.NOT_TAKEN){
@@ -126,43 +118,60 @@ export default props =>{
         setVisibleDoses(visible)
     }
 
+    /**
+     * Verifica se a dose selecionada é do mesmo dia ou foi tomada 
+     * no dia atual para exibição da modal de ajuste de dose.
+     * @param {*} dose A dose selecionada.
+     */
     const onPressDose = dose =>{
         if(moment().isSame(dose.date, 'day') || moment().isSame(dose.dateTaken, 'day')){
             setEditedDose(dose)
             setShowEditDoseModal(true)
         }
     }
-
-    const shiftDate = offset =>{
-        let newDate = new Date(filterDay)
-        newDate.setDate(newDate.getDate() + offset)
-        setFilterDay(newDate)
+    
+    /**
+     * Atualiza a opção de filtro selecionada e fecha o painel do filtro.
+     * @param {String} value O valor selecionado
+     */
+    const onChangeDoseFilterValue = (value) =>{
+        setFilterOption(value)
+        setShowFilter(false)
     }
-
-    const takeQuickDose = dose =>{
-        dose.status = doseStatus.TOMADA
-        dose.dateTaken = new Date()
-        updateDose(dose, doseActions.TOMAR_DOSE)
+        
+    /**
+     * Atualiza a data filtrada e filtra as doses de acordo com a data.
+     * @param {*} date A data selecionada
+     */
+    const onChangeDate = (date) =>{
+        setFilterDay(date)
+        filterDoses()
     }
-
+        
+    /**
+     * Direciona a dose editada para ser atualiza de acordo com 
+     * o que foi feito na modal de ajuste de dose.
+     * @param {*} dose A dose a ser atualizada
+     */
     const onEditDose = dose =>{
-        switch(dose.status){
-            case doseStatus.TOMADA:
-                updateDose(dose, doseActions.EDITAR_DOSE_TOMADA)
-                break
-            case doseStatus.NAO_TOMADA:
-                if(dose.newDate && !dose.dateTaken){
-                    updateDose(dose, doseActions.EDITAR_DOSE_NAO_TOMADA)    
-                }else{
-                    updateDose(dose, doseActions.TOMAR_DOSE)
-                }
-                break
+        if(dose.status ==  doseStatus.TOMADA){
+            updateDose(dose, doseActions.EDITAR_DOSE_TOMADA)
+        }else if(dose.status == doseStatus.NAO_TOMADA){
+            if(dose.newDate && !dose.dateTaken){
+                updateDose(dose, doseActions.EDITAR_DOSE_NAO_TOMADA)    
+            }else{
+                updateDose(dose, doseActions.TOMAR_DOSE)
+            }
         }
         setShowEditDoseModal(false)
     }
 
+    /**
+     * Atualiza uma dose para persistencia de acordo com a ação definida.
+     * @param {*} dose A dose que deve ser atualizada.
+     * @param {Number} action A ação a ser tomada.
+     */
     const updateDose = (dose, action) =>{
-
         var medList = [...meds]
         var updatedMed = medList.filter(m => m.name == dose.medName)[0]
         var updatedDose = updatedMed.doses.filter(d => dose.medName == d.medName && d.index == dose.index )[0]
@@ -182,171 +191,11 @@ export default props =>{
                     break
             }
         }
-        newDoses = updatedMed.doses.map( d => d.index == dose.index ? dose : d)
-        updatedMed.doses = newDoses
 
-        if(swipeableRef && swipeableRef.current){
-            swipeableRef.current.close()
-        }
+        updatedMed.doses = updatedMed.doses.map( d => d.index == dose.index ? dose : d) 
+
         setMeds(medList)
         updateMeds()
-             
-    }
-
-    const getDateFilter = () =>{
-        let datePicker = <DateTimePicker value={filterDay}
-        
-        onChange ={ (event, date) => {
-            setShowDatePicker(false)
-            if(event.type == "set"){
-                setFilterDay(date)
-                filterDoses()
-            }
-        }}
-        mode='date'/>
-        let showReset = !moment().isSame(filterDay, 'date')
-        var dateString = moment(filterDay).isSame(moment(), 'date') ? 'Hoje, ' : moment(filterDay).format('ddd, ')
-        dateString += moment(filterDay).format('D/MM/YY')
-        datePicker = (
-            <DateFilterContainer>
-                <DatePickerView>
-                    <TouchableOpacity style={{marginTop: showReset ? 20 : 12}} onPress={() => shiftDate(-1)}>
-                        <Icon name="leftcircle" type="ant-design" size={32} color={colors.white}/>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={showReset ? {} : {marginTop: 10}} onPress={ () => setShowDatePicker(true)}>
-                            <DateText>{dateString}</DateText>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={{marginTop: showReset ? 20 : 12}} onPress={() => shiftDate(1)}>
-                        <Icon name="rightcircle" type="ant-design" size={32} color={colors.white}/>
-                    </TouchableOpacity>
-                </DatePickerView>
-                {showReset &&
-                <ResetDateButton>
-                    <TouchableOpacity onPress={() => setFilterDay(new Date())}>
-                        <RowView>
-                            <Icon name="calendar-refresh" type="material-community" size={22} color={colors.white}/>
-                            <ResetDatetext>
-                                Hoje
-                            </ResetDatetext>
-                        </RowView>
-                    </TouchableOpacity>
-                </ResetDateButton>
-                }
-                {showDatePicker && datePicker}
-            </DateFilterContainer>
-        )
-        return datePicker
-    }
-
-    const getRightSwipe = (dose) => {
-        if (false && dose.status == doseStatus.NAO_TOMADA && moment().isSame(dose.date, 'day')){
-            return (
-                <RightSwipe style={{backgroundColor: colors.ok}}>
-                    <RightSwipeText>
-                        Tomar dose
-                    </RightSwipeText>
-                    <Icon name="check" size={30} color={colors.white}/>
-                </RightSwipe>
-            )
-        }   
-    }
-
-    const getDoseItem = ({item : dose}) =>{
-        var now = moment()
-        var status = null
-        switch(dose.status){
-            case doseStatus.CANCELADA:
-                status = (
-                    <LightText>
-                        Cancelada
-                    </LightText>
-                )
-                break
-            case doseStatus.NAO_TOMADA:
-                if(moment(dose.date).isBefore(now, 'minute') ){
-                    status = (
-                        <RowView>
-                            <HPadding>
-                                <WarningText>
-                                    {moment(dose.date).format('HH:mm')}
-                                </WarningText>
-                            </HPadding>
-                            <Icon name="warning" type={"font-awesome"} size={24} color={colors.alert}/>
-                        </RowView>
-                    )
-                }else{
-                    status = (
-                        <RowView>
-                            <HPadding>
-                                <WaitingText>
-                                    {moment(dose.date).format('HH:mm')}
-                                </WaitingText>
-                            </HPadding>
-                            <Icon name="clock-o" type="font-awesome" size={24} color={colors.grey6}/>
-                        </RowView>
-                    )
-                }
-                break
-            case doseStatus.TOMADA:
-                status = (
-                    <RowView>
-                        <HPadding>
-                            <OkText>
-                                {moment(dose.dateTaken).format('HH:mm')}
-                            </OkText>
-                        </HPadding>
-                        <Icon name="check-circle" type={"font-awesome"} size={24} color={colors.ok}/>
-                    </RowView>
-            )
-                break
-            case doseStatus.ADIADA:
-                status = (
-                    <LightText>
-                        Adiado para {moment(dose.newDate).format('HH:mm')}
-                    </LightText>
-                )
-                break
-            case doseStatus.PAUSADA:
-                status = (
-                    <LightText>
-                        Tratamento pausado
-                    </LightText>
-                )
-                break
-        }
-
-        return(
-            <>
-            <TouchableOpacity
-                activeOpacity={0.3}
-                onPress={() => onPressDose(dose)}>
-                <Swipeable
-                    ref={swipeableRef}
-                    renderRightActions={() => getRightSwipe(dose)}
-                    onSwipeableRightOpen={() => takeQuickDose(dose)}>
-                    <CardBox>
-                        <ColorTag style={{backgroundColor: dose.iconColor}}/>
-                        <Detail>
-                            <TopContent>
-                                <MedIcon name={dose.icon} size={24} color={dose.iconColor}/> 
-                                <DarkText numberOfLines={1}>
-                                    {dose.medName}
-                                </DarkText>
-                            </TopContent>
-                            <BottomContent>
-                                <LightText>
-                                    {dose.amount} {dose.unit.label}{dose.amount > 1 && dose.unit.label != "Ml" ? "s" : ""}
-                                </LightText>
-                            </BottomContent>
-                        </Detail>
-                        <Buttons>
-                            {status}
-                        </Buttons>
-                    </CardBox>
-                </Swipeable>
-            </TouchableOpacity>
-            </>
-        )
     }
 
     return(
@@ -357,45 +206,34 @@ export default props =>{
                 visible={showEditDoseModal}
                 dose={editedDose}
                 onSet={onEditDose}
-                close={closeEditModal}
+                close={() => setShowEditDoseModal(false)}
             />}
+            <DoseFilter
+                visible={showFilter}
+                options={getFilterOptions()}
+                title={"Filtrar Doses"}
+                storageKey={storageKeys.HOME_FILTER}
+                onClose={() => setShowFilter(false)}
+                value={1}
+                onChangeValue={onChangeDoseFilterValue}/>
             <Header 
                 title="Doses do Dia"
                 rightButton="filter"
-                onPressRight={() => console.warn("pressed")}
+                onPressRight={() => setShowFilter(true)}
                 onPressLeft={() => props.navigation.toggleDrawer()}/>
             <TopView>
-            {getDateFilter()}
-                <Body>
-                    {visibleDoses && visibleDoses.length > 0 &&
-                    <FlatList
-                        data={visibleDoses}
-                        keyExtractor={ (item, index) => `${index}`}
-                        renderItem={getDoseItem}
-                        refreshControl={
-                            <RefreshControl
-                                refreshing={refreshing}
-                                onRefresh={onRefresh}/>
-                        }/>
-                    }
-                    {!visibleDoses || visibleDoses.length == 0 &&
-                    <EmptyListContainer>
-                        <LightText>
-                            Nenhuma dose para a data selecionada!
-                        </LightText>
-                    </EmptyListContainer>
-                    }
-                </Body>
+                <DateFilter
+                onChangeDate={onChangeDate}/>
+                <DoseList 
+                    visibleDoses={visibleDoses}
+                    onPressDose={onPressDose}/>
             </TopView>  
             <PrimaryButton
                 bottom
                 text="Nova Dose"
                 visible={meds.length > 0 && moment().isSameOrAfter(filterDay, 'date')}
                 onClick={navigateToNew}/>
-
         </Container>
-
     )
-
 }
    
