a8c7c40f83ec3bcc22deeb66b0b6b25a95a44f69
ddszb <zambranobrasil@gmail.com>
2022-02-12 18:45:41 -0300

    Componente: Picker >> typescript

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import ActionButton from './ActionButton';
 import Filter from './Filter';
 import Header from './Header';
+import Picker from './Picker';
 
-export {ActionButton, Filter, Header};
+export {ActionButton, Filter, Header, Picker};
