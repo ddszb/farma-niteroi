b244cda3e9962583f3d6db017ede906d47441d39
ddszb <zambranobrasil@gmail.com>
2022-02-13 17:47:25 -0300

    Modelo para  Medicamento Anvisa

---
+++ src/Models/MedAnvisa.ts
@@ -0,0 +1,17 @@
+export default class MedAnvisa {
+	drug: string;
+	holder: string;
+	popularName: string;
+	registry: string;
+	concentration: string;
+	presentation: string;
+
+	constructor() {
+		this.drug = '';
+		this.holder = '';
+		this.popularName = '';
+		this.registry = '';
+		this.concentration = '';
+		this.presentation = '';
+	}
+}
